828744ba60c49e6a2fe305c42e84535603bf3fa5
root <root@ip-172-31-22-115.ec2.internal>
2021-09-12 12:35:42 +0000

    This is First Commit

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import ReactTable from 'react-table'
 import api from '../api'
-
+import ReactTable from "react-table-6";
+import "react-table-6/react-table.css"
 import styled from 'styled-components'
-
-import 'react-table/react-table.css'
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
